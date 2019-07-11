449ce8786947df1513ee3d05565f36071318b8bf
Robin Böhm <robin.boehm@workshops.de>
2019-07-11 15:23:15 +0200

    feat(orm): add basic typeorm config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common';
+
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { EventsModule } from './events/events.module';
 
 @Module({
-  imports: [EventsModule],
+  imports: [
+    TypeOrmModule.forRoot(),
+    EventsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
