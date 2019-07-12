fa0f0cd3066bd6f5183bbe2445ee9699ad7d596f
Robin Böhm <robin.boehm@workshops.de>
2019-07-12 17:56:18 +0200

    feat(event): add basic database connection

--- src/events/events.controller.ts
+++ src/events/events.controller.ts
@@ -1,5 +1,6 @@
 import { Controller, Get, Param } from '@nestjs/common';
 import { EventsService } from './events.service';
+import { Event } from './event.entity';
 
 @Controller('events')
 export class EventsController {
@@ -7,12 +8,12 @@ export class EventsController {
     constructor(private eventService: EventsService) {}
 
     @Get()
-    findAll(): any[] {
+    findAll(): Promise<Event[]> {
         return this.eventService.findAll();
     }
 
     @Get(':id')
     findOne(@Param('id') id: string): any {
-      return this.eventService.findAll()[0];
+      return this.eventService.findOne(id);
     }
 }
--- src/events/events.service.ts
+++ src/events/events.service.ts
@@ -1,14 +1,22 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Event } from './event.entity';
+import { Repository } from 'typeorm';
+
 
 @Injectable()
 export class EventsService {
 
-    findAll(): any[] {
-        return [
-            {
-                id: 1,
-                name: 'Angular.DE Intensiv Schulung',
-            },
-        ];
+    constructor(
+        @InjectRepository(Event)
+        private readonly eventRepository: Repository<Event>,
+      ) {}
+
+    findAll(): Promise<Event[]> {
+        return this.eventRepository.find();
+    }
+
+    findOne(id: string): Promise<Event> {
+        return this.eventRepository.findOne(id);
     }
 }
